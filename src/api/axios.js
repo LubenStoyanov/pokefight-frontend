d6873044ae07a4b10d557b12b4f1a3ec58e37d6b
Luben <luben.stoyanov.ls@gmail.com>
2022-11-04 19:44:58 +0100

    Remove redundant async

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-export const getAllPokemons = async () =>
+export const getAllPokemons = () =>
   axios
     .get(import.meta.env.VITE_API_URL + "pokemons")
     .then((res) => res.data)
     .catch((error) => console.error(error));
 
-export const getOnePokemon = async (id) =>
+export const getOnePokemon = (id) =>
   axios
     .get(import.meta.env.VITE_API_URL + `pokemons/${id}`)
     .then((res) => res.data)
     .catch((error) => console.error(error));
 
-export const getInfo = async (id, info) =>
+export const getInfo = (id, info) =>
   axios
     .get(import.meta.env.VITE_API_URL + `pokemons/${id}/${info}`)
     .then((res) => res.data)
