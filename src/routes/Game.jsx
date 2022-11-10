7a23df36959d3b5d5915d43fb100e7c0445f15ea
Marina Ilyushina <m_ilyushina@hotmail.com>
2022-11-10 10:27:38 +0100

    styles game pg

--- src/routes/Game.jsx
+++ src/routes/Game.jsx
@@ -1,14 +1,46 @@
-import { Button, Heading } from "@chakra-ui/react";
+import { Box, Button, Heading,  } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Game() {
+
   return (
     <div>
-      <Heading>Pokéfight</Heading>
-      <Link to={"/select"}>
-        <Button>Start</Button>
-      </Link>
+   <Box    
+      maxW="100%"
+      height="100vh"
+      display="flex" 
+      alignItems="center" 
+      justifyContent="center" 
+      flexDirection="column"
+      rowGap={5}
+      backgroundImage="url('icons/game.jpg')"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat" 
+
+      >
+        <Heading
+          fontFamily="Nabla"
+          fontSize='4em'
+        >Pokéfight</Heading>
+        <Link to={"/select"}>
+          <Button
+            fontFamily="Orbitron"
+            letterSpacing={3}
+            bg="#FFE433"
+            color="#1F2231"
+            boxShadow='md'
+            rounded='md'
+            _hover={{
+              background: "#01DEE6",
+              color: "#1F2231",
+            }}
+            _focus={{
+              background: "#01DEE6",
+            }}
+          >Start</Button>
+        </Link>
+      </Box>
     </div>
   );
 }
