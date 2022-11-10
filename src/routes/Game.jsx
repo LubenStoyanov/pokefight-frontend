b5d64da3503e32bbe4b96d976f89baf42ac3db10
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2022-11-10 17:34:35 +0100

    Changes

--- src/routes/Game.jsx
+++ src/routes/Game.jsx
@@ -1,39 +1,40 @@
-import { Box, Button, Heading,  } from "@chakra-ui/react";
+import { Box, Button, Heading } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Game() {
-
   return (
     <div>
-   <Box    
-      maxW="100%"
-      height="100vh"
-      display="flex" 
-      alignItems="center" 
-      justifyContent="center" 
-      flexDirection="column"
-      rowGap={5}
-      backgroundImage="url('icons/game.jpg')"
-      backgroundPosition="center"
-      backgroundRepeat="no-repeat" 
-
+      <Box
+        maxW="100%"
+        height="100vh"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        rowGap={5}
+        backgroundImage="url('icons/game.jpg')"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        backgroundSize="cover"
       >
         <Heading
           fontFamily="Pokemon Solid"
-          fontSize='4em'
+          fontSize="4em"
           color="#fff"
           letterSpacing={7}
           textShadow="2px 2px 4px #ff0000"
-        >Pokéfight</Heading>
+        >
+          Pokéfight
+        </Heading>
         <Link to={"/select"}>
           <Button
             fontFamily="ARCADECLASSIC"
             letterSpacing={3}
             bg="#ffcc03"
             color="#1F2231"
-            boxShadow='md'
-            rounded='md'
+            boxShadow="md"
+            rounded="md"
             mt={5}
             fontSize={20}
             _hover={{
@@ -42,9 +43,11 @@ export default function Game() {
             }}
             _focus={{
               background: "#3c5aa6",
-              color: "#fff"
+              color: "#fff",
             }}
-          >Start</Button>
+          >
+            Start
+          </Button>
         </Link>
       </Box>
     </div>
