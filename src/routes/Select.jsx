cb83c6ec0ad9e7b721bf030cee2ad8e3718ea004
Marina Ilyushina <m_ilyushina@hotmail.com>
2022-11-10 11:18:48 +0100

    some style changes

--- src/routes/Select.jsx
+++ src/routes/Select.jsx
@@ -7,7 +7,14 @@ import Searchbar from "../components/Searchbar";
 export default function Select() {
   return (
     <div>
-      <Heading as="h1">Select</Heading>
+      <Heading 
+        as="h1"
+        fontFamily="Pokemon Solid"
+        fontSize='4em'
+        color="#fff"
+        letterSpacing={7}
+        textShadow="2px 2px 4px #ff0000"
+        >Select</Heading>
       <Link to={`/select/pokemons/All`}>
         <Button>All</Button>
       </Link>
