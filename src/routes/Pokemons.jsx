85c731e36a2716a505cd44ed3fbe412aecd921d4
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2022-11-11 08:10:53 +0100

    Handle empty search, not found and remove Pokemon.jsx

--- src/routes/Pokemons.jsx
+++ src/routes/Pokemons.jsx
@@ -24,40 +24,36 @@ export default function Pokemons() {
   //   console.log(searchPokemon);
   // }, [searchPokemon]);
   return (
-    <Wrap
-      display="flex"
-      justify="center"
-      maxW="65%"
-    >
+    <Wrap display="flex" justify="center" maxW="65%">
       {pokemons.map((p) => (
-        <Link
-        to={`/select/pokemons/id/${p.id}`} 
-        key={p.id}>
-          <VStack 
-          backgroundColor="#0075BE"
-          border="solid"
-          borderColor="#ffcc03"
-          borderWidth="1px"
-          borderRadius="15px"
-          boxShadow="md"
-          boxSize="300px"
-          display="flex"
-          justify="center">
-            <Heading 
-            p="5px"
-            as="h2" size="md"
-            fontFamily='ARCADECLASSIC'
-            letterSpacing={3}
-            color="#ffcc03">
+        <Link to={`/select/pokemons/id/${p.id}`} key={p.id}>
+          <VStack
+            backgroundColor="#0075BE"
+            border="solid"
+            borderColor="#ffcc03"
+            borderWidth="1px"
+            borderRadius="15px"
+            boxShadow="md"
+            boxSize="300px"
+            display="flex"
+            justify="center"
+          >
+            <Heading
+              p="5px"
+              as="h2"
+              size="md"
+              fontFamily="ARCADECLASSIC"
+              letterSpacing={3}
+              color="#ffcc03"
+            >
               {p.name.english}
             </Heading>
             <Box boxSize="xs">
               <Image
                 src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${p.id}.png`}
-                 boxSize="150px"
+                boxSize="150px"
               />
-              <Button
-              >Click</Button>
+              <Button>Click</Button>
             </Box>
           </VStack>
         </Link>
